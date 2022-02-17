b152f44631b13d68dc930bb4dc91b9661f9ae45a
Avivas25 <76022422+Avivas25@users.noreply.github.com>
2022-02-16 16:12:04 -0800

    Added DInjectionWipe1 Scene

---
+++ Assets/Scripts/Intro172.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro172: MonoBehaviour
+{
+    public void practiceSceneCreation()
+    {
+        SceneManager.LoadScene("Intro17");
+    }
+}
