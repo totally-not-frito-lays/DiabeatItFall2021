fb63390e9640c1743368c6e9ef94130b386160e8
rtapia1 <rtapia@ucsd.edu>
2022-03-17 15:13:38 -0700

    DInsulinRot 4-7 AND SCENE TRANSITION

---
+++ Assets/Scripts/DInsulinRot7.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DInsulinRot7 : MonoBehaviour
+{
+    public void sceneDInsulinRot7Transition( string name)
+    {
+        SceneManager.LoadScene(name);
+    }
+}
+
